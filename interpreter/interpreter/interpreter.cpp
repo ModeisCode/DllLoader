47629b1a6bc98013b0ca7d7751c56e797c4ee1fc
SoftwareArchitecture <100340196+ModeisCode@users.noreply.github.com>
2024-02-14 15:39:03 +0300

    Update interpreter.cpp

--- interpreter/interpreter/interpreter.cpp
+++ interpreter/interpreter/interpreter.cpp
@@ -14,29 +14,3 @@ int main() {
     return 0;
 }
 
-/*    
-    // DLL'yi yükle
-
-    HMODULE dll = LoadLibrary(L"C:\\Users\\doguk\\OneDrive\\Masaüstü\\DEVLAB\\CSHARP\\interpreter\\interpreter\\mydll.dll");
-    if (dll == NULL) {
-        std::cerr << "DLL yüklenemedi." << std::endl;
-        return 1;
-    }
-
-    // Fonksiyon adresini al
-    int (*add)(int, int) = (int (*)(int, int))GetProcAddress(dll, "add");
-    if (add == NULL) {
-        std::cerr << "Fonksiyon adresi alınamadı." << std::endl;
-        return 1;
-    }
-
-    // Fonksiyonu çağır
-    int result = add(10, 20);
-
-    // Sonucu yazdır
-    std::cout << "Sonuç: " << result << std::endl;
-
-
-    // DLL'yi kaldır
-    FreeLibrary(dll);
-*/
